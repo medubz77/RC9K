77db5c8f50fb27e509398e14fe3953865f0f20f6
Waldo <waldoxwho@yahoo.com>
2017-02-12 17:05:21 -0500

    just.. you know.. doing stuff.

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,3 @@ require 'active_record'
 require 'yaml'
 require 'net/ping'
 require 'action_view'
-# require 'piface'
-#require File.join(File.dirname(__FILE__),'db_connect.rb')
-#ActiveRecord::Base.logger = Logger.new(File.open('log/migrate_database.log', 'a'))
--- testing.rb
+++ testing.rb
@@ -1,10 +1,9 @@
-require File.join(File.dirname(__FILE__),'config/application.rb')
+#require File.join(File.dirname(__FILE__),'config/application.rb')
 class DoSomething
   def initialize
 
   end
-  def self.do_this(action)
-
+  def do_this(action)
     puts "Look at me, I'm #{action}"
   end
 end
