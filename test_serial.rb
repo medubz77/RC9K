88cfc0c7b368557f54b437b7035452661f057aab
Waldo Ramirez <waldoxwho@yahoo.com>
2017-02-23 21:31:51 -0500

    modded the test_serial.rb to move legs

--- test_serial.rb
+++ test_serial.rb
@@ -3,19 +3,72 @@ require 'rubyserial'
 class TestSerial
 # Instantiate serial
 def initialize
-@@my_serial = Serial.new("/dev/ttyACM1", baude_rate = 115200, data_bits = 8)
+#@@my_serial = Serial.new("/dev/ttyACM0", baude_rate = 115200, data_bits = 8)
+@@my_serial = Serial.new("/dev/ttyACM0", baude_rate = 115200)
 end
 # Get data and do whatever you want with it
 def lookie
 size = 1024
 data = @@my_serial.read(size)
 end
-# Set data
+
+# Set leg and positions for manual moving of the leg(s).
 def move(leg_num, pos_1, pos_2, pos_3)
 # data = ""
 data = "Leg(#{leg_num},#{pos_1},#{pos_2},#{pos_3})"
 @@my_serial.write(data)
 end
+
+def move_legs(position)
+
+@@legs = [0,1,2,3,4,5]
+
+case position
+
+when "stand"
+legs = [0,1,2,3,4,5]
+@@legs.each do |num|
+leg = "Leg(#{num},150,180,165)"
+puts leg
+@@my_serial.write(leg)
+
+end
+
+when "laydown"
+@@legs.each do |num|
+leg = "Leg(#{num},150,120,120)"
+puts leg
+@@my_serial.write(leg)
+
+end
+
+when "wave"
+
+alt_legs = [0,2,4,5,3,1]
+alt_legs.each do |num|
+leg = "Leg(#{num},150,180,165)"
+puts leg
+@@my_serial.write(leg)
+sleep 0.25
+end
+sleep 1
+
+alt_legs = [1,3,5,4,2,0]
+alt_legs.each do |num|
+leg = "Leg(#{num},150,120,120)"
+puts leg
+@@my_serial.write(leg)
+sleep 0.25
+
+end
+
+end
+
+end
+
+
+
+# method to make rc9k dance
 def dance
 # data = ""
 counter = [0,1,2,3,4,5]
@@ -44,6 +97,7 @@ data_reset = "Leg(#{leg_num},#{pos_1},#{pos_2},#{pos_3})"
 puts data_reset
 sleep 1
 end
+
 end
 # Close serial once you're done using it
 def stop_serial
