9902981373ce6b1aa12b53bac92cdbef45549a37
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-03 21:53:24 -0400

    remove extra = sign

--- crab.rb
+++ crab.rb
@@ -119,7 +119,7 @@ end
 
 def left_swing_down
 	down_set = [$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d]
-	down_side = = [$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d]
+	down_side = [$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d]
 	writetolegs(down_side, 1, 0)
 	sleep 0.25
 	writetolegs(down_set, 1, 0)
