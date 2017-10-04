26b62170bd69cb67bfff0ce19c98994a2a2f4351
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-04 19:52:28 -0400

    debuggin

--- crab.rb
+++ crab.rb
@@ -116,12 +116,12 @@ def left_swing_up
 	lg4_d = [$j1r,$j2d-20,$j3d-20]
 	lg5_d = [$j1r,$j2d-20,$j3d-20]
 	#up_side = [$j1r,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20]
-	lg0_u = [$j1r,$j2d+20,$j3d+20]
-	lg1_u = [$j1r,$j2d+20,$j3d+20]
+	lg0_u = [$j1r,$j2d,$j3d]
+	lg1_u = [$j1r,$j2d,$j3d]
 	lg2_u = [$j1l,$j2d-20,$j3d+20]
 	lg3_u = [$j1l,$j2d-20,$j3d+20]
-	lg4_u = [$j1r,$j2d+20,$j3d+20]
-	lg5_u = [$j1r,$j2d+20,$j3d+20]
+	lg4_u = [$j1r,$j2d,$j3d]
+	lg5_u = [$j1r,$j2d,$j3d]
 	#up_set = [$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d,$j3d,$j1l,$j2d,$j3d,$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d]
 	writetolegs(lg0_d + lg1_d + lg2_d + lg3_d + lg4_d + lg5_d, 1, 0)
 	sleep 0.25
@@ -137,12 +137,12 @@ def left_swing_down
 	lg4_d = [$j1l,$j2d-20,$j3d-20]
 	lg5_d = [$j1l,$j2d-20,$j3d-20]
 	#down_set = [$j1l,$j2d-20,$j3d-20,$j1l,$j2d-20,$j3d-20,$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d-20,$j3d-20,$j1l,$j2d-20,$j3d-20]
-	lg0_u = [$j1l,$j2d+20,$j3d+20]
-	lg1_u = [$j1l,$j2d+20,$j3d+20]
+	lg0_u = [$j1l,$j2d,$j3d]
+	lg1_u = [$j1l,$j2d,$j3d]
 	lg2_u = [$j1r,$j2d-20,$j3d-20]
 	lg3_u = [$j1r,$j2d-20,$j3d-20]
-	lg4_u = [$j1l,$j2d+20,$j3d+20]
-	lg5_u = [$j1l,$j2d+20,$j3d+20]
+	lg4_u = [$j1l,$j2d,$j3d]
+	lg5_u = [$j1l,$j2d,$j3d]
 	#down_side = [$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d,$j3d,$j1r,$j2d,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d,$j3d]
 	writetolegs(lg0_d + lg1_d + lg2_d + lg3_d + lg4_d + lg5_d, 1, 0)
 	sleep 0.25
