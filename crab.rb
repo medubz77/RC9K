e9b4931b2db5031d8fd35ca98d75793ab899e735
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-03 21:43:50 -0400

    complete walk cycle debug

--- crab.rb
+++ crab.rb
@@ -118,8 +118,8 @@ end
 
 
 def left_swing_down
-	up_side = [$j1l,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20]
-	up_set = [$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d]
+	down_side = [$j1l,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20]
+	down_set = [$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d]
 	writetolegs(down_side, 1, 0)
 	sleep 0.25
 	writetolegs(down_set, 1, 0)
