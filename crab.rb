8ac7725bea1ca10c03ef14482896b50fb72ebe11
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-03 22:05:05 -0400

    still testing walk cycle

--- crab.rb
+++ crab.rb
@@ -110,7 +110,7 @@ end
 
 def left_swing_up
 	up_side = [$j1r,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d-20,$j3d-20,$j1r,$j2d-20,$j3d-20]
-	up_set = [$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d]
+	up_set = [$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d,$j3d,$j1l,$j2d,$j3d,$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d]
 	writetolegs(up_side, 1, 0)
 	sleep 0.25
 	writetolegs(up_set, 1, 0)
@@ -118,8 +118,8 @@ end
 
 
 def left_swing_down
-	down_set = [$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d]
-	down_side = [$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d+20,$j3d]
+	down_set = [$j1l,$j2d-20,$j3d-20,$j1l,$j2d-20,$j3d-20,$j1r,$j2d+20,$j3d,$j1r,$j2d+20,$j3d,$j1l,$j2d-20,$j3d-20,$j1l,$j2d-20,$j3d-20]
+	down_side = [$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d,$j1r,$j2d,$j3d,$j1r,$j2d,$j3d,$j1l,$j2d+20,$j3d,$j1l,$j2d+20,$j3d]
 	writetolegs(down_side, 1, 0)
 	sleep 0.25
 	writetolegs(down_set, 1, 0)
