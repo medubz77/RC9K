1496e31690c1ea6b90a0d06c138f7fdbfd14b54a
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-04 22:14:35 -0400

    one mo' again

--- crab.rb
+++ crab.rb
@@ -132,6 +132,18 @@ end
 
 def swing_down
 	##left
+	lg0 = [$j1l,$j2d-20,$j3d-20]
+	lg3 = [$j1r,$j2d-20,$j3d-20]
+	lg4 = [$j1l,$j2d-20,$j3d-20]
+	##right
+	lg1 = [$j1l,$j2d+20,$j3d]
+	lg2 = [$j1r,$j2d+20,$j3d]
+	lg5 = [$j1l,$j2d+20,$j3d]
+
+	writetolegs(lg0 + lg1 + lg2 + lg3 + lg4 + lg5, 1, 0)
+end
+def swing_down_set
+	##left
 	lg0 = [$j1l,$j2d+20,$j3d]
 	lg3 = [$j1r,$j2d+20,$j3d]
 	lg4 = [$j1l,$j2d+20,$j3d]
@@ -139,18 +151,6 @@ def swing_down
 	lg1 = [$j1l,$j2d-20,$j3d-20]
 	lg2 = [$j1r,$j2d-20,$j3d-20]
 	lg5 = [$j1l,$j2d-20,$j3d-20]
-
-	writetolegs(lg0 + lg1 + lg2 + lg3 + lg4 + lg5, 1, 0)
-end
-def swing_down_set
-	##left
-	lg0 = [$j1r,$j2d-20,$j3d-20]
-	lg3 = [$j1l,$j2d-20,$j3d-20]
-	lg4 = [$j1r,$j2d-20,$j3d-20]
-	##right
-	lg1 = [$j1l,$j2d+20,$j3d]
-	lg2 = [$j1r,$j2d+20,$j3d]
-	lg5 = [$j1l,$j2d+20,$j3d]
 	writetolegs(lg0 + lg1 + lg2 + lg3 + lg4 + lg5, 1, 0)
 end
 
