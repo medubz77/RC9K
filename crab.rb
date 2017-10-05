a1efe49cb5f9341b47650e04dd12c77bbda2c5e8
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-04 21:02:29 -0400

    debugging

--- crab.rb
+++ crab.rb
@@ -109,17 +109,17 @@ end
 
 def left_swing_up
 	lg0_u = [$j1r,$j2d,$j3d]
-	lg3_u = [$j1r,$j2d,$j3d]
+	lg3_u = [$j1l,$j2d,$j3d]
 	lg4_u = [$j1r,$j2d,$j3d]
 	lg1_u = [$j1l,$j2d,$j3d]
-	lg2_u = [$j1l,$j2d,$j3d]
+	lg2_u = [$j1r,$j2d,$j3d]
 	lg5_u = [$j1l,$j2d,$j3d]
 
 	lg0_d = [$j1r,$j2d,$j3d]
-	lg3_d = [$j1r,$j2d,$j3d]
+	lg3_d = [$j1l,$j2d,$j3d]
 	lg4_d = [$j1r,$j2d,$j3d]
 	lg1_d = [$j1l,$j2d,$j3d]
-	lg2_d = [$j1l,$j2d,$j3d]
+	lg2_d = [$j1r,$j2d,$j3d]
 	lg5_d = [$j1l,$j2d,$j3d]
 
 	writetolegs(lg0_u + lg1_u + lg2_u + lg3_u + lg4_u + lg5_u, 1, 0)
@@ -131,17 +131,17 @@ end
 
 def left_swing_down
 	lg0_d = [$j1l,$j2d,$j3d]
-	lg3_d = [$j1l,$j2d,$j3d]
+	lg3_d = [$j1r,$j2d,$j3d]
 	lg4_d = [$j1l,$j2d,$j3d]
 	lg1_d = [$j1r,$j2d,$j3d]
-	lg2_d = [$j1r,$j2d,$j3d]
+	lg2_d = [$j1l,$j2d,$j3d]
 	lg5_d = [$j1r,$j2d,$j3d]
 
 	lg0_u = [$j1l,$j2d,$j3d]
-	lg3_u = [$j1l,$j2d,$j3d]
+	lg3_u = [$j1r,$j2d,$j3d]
 	lg4_u = [$j1l,$j2d,$j3d]
 	lg1_u = [$j1r,$j2d,$j3d]
-	lg2_u = [$j1r,$j2d,$j3d]
+	lg2_u = [$j1l,$j2d,$j3d]
 	lg5_u = [$j1r,$j2d,$j3d]
 	writetolegs(lg0_d + lg1_d + lg2_d + lg3_d + lg4_d + lg5_d, 1, 0)
 	sleep 0.5
