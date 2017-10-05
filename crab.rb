099a7f1b1a4467fc46500b63b5cd4d608d0561bb
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-04 21:10:05 -0400

    ssssss

--- crab.rb
+++ crab.rb
@@ -109,7 +109,7 @@ end
 
 def left_swing_up
 	lg0_u = [$j1r,$j2d,$j3d]
-	lg3_u = [$j1l,$j2d,$j3d]
+	lg3_u = [$j1r,$j2d,$j3d]
 	lg4_u = [$j1r,$j2d,$j3d]
 	lg1_u = [$j1l,$j2d,$j3d]
 	lg2_u = [$j1r,$j2d,$j3d]
@@ -131,7 +131,7 @@ end
 
 def left_swing_down
 	lg0_d = [$j1l,$j2d,$j3d]
-	lg3_d = [$j1r,$j2d,$j3d]
+	lg3_d = [$j1l,$j2d,$j3d]
 	lg4_d = [$j1l,$j2d,$j3d]
 	lg1_d = [$j1r,$j2d,$j3d]
 	lg2_d = [$j1l,$j2d,$j3d]
