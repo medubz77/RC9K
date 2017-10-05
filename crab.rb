237ec9af032777985e400030391423e5655f7079
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-04 22:05:44 -0400

    inching along

--- crab.rb
+++ crab.rb
@@ -132,9 +132,9 @@ end
 
 def swing_down
 	##left
-	lg0 = [$j1l,$j2d,$j3d]
-	lg3 = [$j1r,$j2d,$j3d]
-	lg4 = [$j1l,$j2d,$j3d]
+	lg0 = [$j1l,$j2d+20,$j3d]
+	lg3 = [$j1r,$j2d+20,$j3d]
+	lg4 = [$j1l,$j2d+20,$j3d]
 	##right
 	lg1 = [$j1l,$j2d-20,$j3d-20]
 	lg2 = [$j1r,$j2d-20,$j3d-20]
@@ -144,13 +144,13 @@ def swing_down
 end
 def swing_down_set
 	##left
-	lg0 = [$j1r,$j2d,$j3d]
-	lg3 = [$j1l,$j2d,$j3d]
-	lg4 = [$j1r,$j2d,$j3d]
+	lg0 = [$j1r,$j2d-20,$j3d-20]
+	lg3 = [$j1l,$j2d-20,$j3d-20]
+	lg4 = [$j1r,$j2d-20,$j3d-20]
 	##right
-	lg1 = [$j1l,$j2d,$j3d]
-	lg2 = [$j1r,$j2d,$j3d]
-	lg5 = [$j1l,$j2d,$j3d]
+	lg1 = [$j1l,$j2d+20,$j3d]
+	lg2 = [$j1r,$j2d+20,$j3d]
+	lg5 = [$j1l,$j2d+20,$j3d]
 	writetolegs(lg0 + lg1 + lg2 + lg3 + lg4 + lg5, 1, 0)
 end
 
