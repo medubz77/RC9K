6ec3006d8c663635e98297c9715b4974477d1d2a
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-04 22:08:59 -0400

    still going..

--- crab.rb
+++ crab.rb
@@ -158,8 +158,12 @@ def walk_cycle(steps)
 	while steps > 0
 		steps = steps - 1
 		swing_up
+		sleep 0.075
+		swing_up_set
 		sleep 0.5
 		swing_down
+		sleep 0.075
+		swing_down_set
 		sleep 0.5
 	end
 	stand
