058d2a332f9250a857ae50363203e7666505fb63
Waldo Ramirez via rc9k <waldoxwho@yahoo.com>
2017-10-11 19:41:29 -0400

    opencv stuff

--- opencv_module.rb
+++ opencv_module.rb
@@ -6,9 +6,9 @@ include OpenCV
 
 window = GUI::Window.new("face detect")
 capture = CvCapture.open
-capture.fps= => 25
-#detector = CvHaarClassifierCascade::load("/home/pi/tmp/opencv/data/haarcascades_GPU/haarcascade_frontalface_alt2.xml")
-detector = CvHaarClassifierCascade::load("/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml")
+
+detector = CvHaarClassifierCascade::load("/home/pi/tmp/opencv/data/haarcascades_GPU/haarcascade_frontalface_alt2.xml")
+#detector = CvHaarClassifierCascade::load("/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml")
 
 loop {
   image = capture.query
@@ -16,5 +16,5 @@ loop {
     image.rectangle! rect.top_left, rect.bottom_right, :color => CvColor::Red
   }
   window.show image
-  break if GUI::wait_key(100)
+  break if GUI::wait_key(10)
 }
