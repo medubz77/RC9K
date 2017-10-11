32658607050196c5084f0b1bfe20f11facfa64ea
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-11 18:55:38 -0400

    cleaning house and opencv configuration

--- opencv_module.rb
+++ opencv_module.rb
@@ -6,7 +6,8 @@ include OpenCV
 
 window = GUI::Window.new("face detect")
 capture = CvCapture.open
-detector = CvHaarClassifierCascade::load("./data/haarcascades/haarcascade_frontalface_alt.xml")
+detector = CvHaarClassifierCascade::load("/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt2.xml")
+#detector = CvHaarClassifierCascade::load("./data/haarcascades/haarcascade_frontalface_alt.xml")
 
 loop {
   image = capture.query
