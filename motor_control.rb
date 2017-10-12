859c0803f9b8cb2080c5e9ab77e8084d701e11df
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-11 21:40:24 -0400

    motor control

--- motor_control.rb
+++ motor_control.rb
@@ -1,14 +1,13 @@
 require 'rpi_gpio'
 RPi::GPIO.set_numbering :board # sets the pin number to the board physical pins
-
-RPi::GPIO.setup 2, :as => :output, :initialize => :low #setting the intial state for pin 2 as low
+RPi::GPIO.setup 3, :as => :output, :initialize => :low #setting the intial state for the pin as low
 class MotorPower
 
   def toggle(state)
     if state == "on"
-      RPi::GPIO.set_high 2
+      RPi::GPIO.set_high 3
     elsif state == "off"
-      RPi::GPIO.set_low 2
+      RPi::GPIO.set_low 3
     end
   end
 end
