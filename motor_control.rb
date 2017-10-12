fe6a738a78a36cf288468cfc034ad56f3977af64
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-11 21:34:59 -0400

    motor power tweaking

--- motor_control.rb
+++ motor_control.rb
@@ -1,13 +1,13 @@
 require 'rpi_gpio'
 RPi::GPIO.set_numbering :board
-RPi::GPIO.setup 3, :as => :output, :initialize => :low #setting the intial state for pin 3 as low
+RPi::GPIO.setup 2, :as => :output, :initialize => :low #setting the intial state for pin 3 as low
 class MotorPower
 
   def toggle(state)
     if state == "on"
-      RPi::GPIO.set_high 3
+      RPi::GPIO.set_high 2
     elsif state == "off"
-      RPi::GPIO.set_low 3
+      RPi::GPIO.set_low 2
     end
   end
 end
