860744fb3b6fc865e3f668c6a78855d37e31c3ca
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-11 20:57:28 -0400

    opencv testing,

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,21 +1,17 @@
-# houghcircle.rb
 require "rubygems"
 require "opencv"
+
 include OpenCV
 
-original_window = GUI::Window.new "original"
-hough_window = GUI::Window.new "hough circles"
+window = GUI::Window.new("face detect")
+capture = CvCapture.open
+#detector = CvHaarClassifierCascade::load("./data/haarcascades/haarcascade_frontalface_alt.xml")
 
-image = IplImage::load "images/stuff.jpg"
-gray = image.BGR2GRAY
-
-result = image.clone
-original_window.show image
-detect = gray.hough_circles(CV_HOUGH_GRADIENT, 2.0, 10, 200, 50)
-puts detect.size
-detect.each{|circle|
-  puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
-  result.circle! circle.center, circle.radius, :color => CvColor::Red, :thickness => 3
+loop {
+  image = capture.query
+#  detector.detect_objects(image).each { |rect|
+#    image.rectangle! rect.top_left, rect.bottom_right, :color => CvColor::Red#
+#  }
+  window.show image
+  break if GUI::wait_key(100)
 }
-hough_window.show result
-GUI::wait_key
