899d48885239bca2b52ce6a45cc995d59fed806a
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-11 21:27:49 -0400

    added RPi::GPIO.set_numbering :board  to motor_control.rb

--- motor_control.rb
+++ motor_control.rb
@@ -1,5 +1,5 @@
 require 'rpi_gpio'
-
+RPi::GPIO.set_numbering :board 
 class MotorPower
   RPi::GPIO.setup 3, :as => :output, :initialize => :low #setting the intial state for pin 3
   def toggle(state)
