e40d4edffaa1a8716339629a1c86b8eff6dc1c5d
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-11 21:24:52 -0400

    added motor control code

---
+++ motor_control.rb
@@ -0,0 +1,12 @@
+require 'rpi_gpio'
+
+class MotorPower
+  RPi::GPIO.setup 3, :as => :output, :initialize => :low #setting the intial state for pin 3 
+  def toggle(state)
+    if state = "on"
+      RPi::GPIO.set_high 3
+    elsif state = "off"
+      RPi::GPIO.set_low 3
+    end
+  end
+end
