6f27c4b2bd33f9e66c97fe15f940d9a58f574358
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-22 18:00:01 -0400

    trying to set fps for opencv

--- opencv_module.rb
+++ opencv_module.rb
@@ -6,12 +6,12 @@ include OpenCV
 window = GUI::Window.new("face detect")
 capture = CvCapture.open
 #detector = CvHaarClassifierCascade::load("./data/haarcascades/haarcascade_frontalface_alt.xml")
-
-loop {
-  image = capture.query
+puts #{"capture.fps"}
+#loop {
+#  image = capture.query
 #  detector.detect_objects(image).each { |rect|
 #    image.rectangle! rect.top_left, rect.bottom_right, :color => CvColor::Red#
 #  }
-  window.show image
-  break if GUI::wait_key(25)
-}
+#  window.show image
+#  break if GUI::wait_key(25)
+#}
