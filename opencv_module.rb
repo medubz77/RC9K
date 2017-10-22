3fb7aeaee6e1bec6bc69868ec7dcadf80429ca04
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-22 18:57:14 -0400

    winging it opencv

--- opencv_module.rb
+++ opencv_module.rb
@@ -53,9 +53,9 @@ while contour
     box = contour.min_area_rect2
     puts "found minimal rectangle with its center at (#{box.center.x.round},#{box.center.y.round}), width of #{box.size.width.round}px, height of #{box.size.height.round} and an angle of #{box.angle.round} degree"
   end
+  window.show cvmat
+  break if GUI::wait_key(100)
   contour = contour.h_next
 end
 #  image = capture.query
-window.show contour
 }
-break if GUI::wait_key(100)
