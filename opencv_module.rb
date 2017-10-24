cdd2fc8d01ffbc76bfd2d21a27032b99a3646b16
Waldo Ramirez via rc9k <waldoxwho@yahoo.com>
2017-10-24 18:07:04 -0400

    tailing file stuff

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,9 +1,11 @@
-
 require 'file/tail'
 
 #filename = ARGV.pop or fail "Usage: #$0 number filename"
 #number = (ARGV.pop || 0).to_i.abs
-filename = "./tmp/tempcamera.txt"
+filename = "/home/pi/rc9k/tmp/tempcamera.txt"
 File::Tail::Logfile.open(filename) do |log|
-  log.forward(10).tail { |line| puts line }
+  log.backward(1).tail { |line| 
+puts line
+puts "#{filename}"
+ }
 end
