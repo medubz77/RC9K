34fb8763703128fb60e68a2a7c8431f6db36eb8a
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-24 19:11:11 -0400

    Rob shares input

--- ball_tracking.py
+++ ball_tracking.py
@@ -45,7 +45,7 @@ while True:
 
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=240)
+	frame = imutils.resize(frame, width=480)
 	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
