c4c29c0b3dd89adec017332d47e57de37fc463d4
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-23 22:41:54 -0400

    opencv test

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,20 +1,22 @@
-require "rubygems"
-require "opencv"
+require 'opencv'
 include OpenCV
 result = exec("python GrabImage.py")
-original_window = GUI::Window.new "original"
-hough_window = GUI::Window.new "hough circles"
 
-image = IplImage::load "image.png"
-gray = image.BGR2GRAY
+if ARGV.length < 2
+  puts "Usage: ruby #{__FILE__} source dest"
+  exit
+end
 
-result = image.clone
-original_window.show image
-detect = gray.hough_circles(CV_HOUGH_GRADIENT, 2.0, 10, 200, 50)
-puts detect.size
-detect.each{|circle|
-  puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
-  result.circle! circle.center, circle.radius, :color => CvColor::Red, :thickness => 3
-}
-hough_window.show result
-GUI::wait_key(100)
+data = './opencv_data/haarcascades/haarcascade_frontalface_alt.xml'
+detector = CvHaarClassifierCascade::load(data)
+#image = CvMat.load(ARGV[0])
+image = CvMat.load("./image.png")
+detector.detect_objects(image).each do |region|
+  color = CvColor::Blue
+  image.rectangle! region.top_left, region.bottom_right, :color => color
+end
+
+image.save_image(ARGV[1])
+window = GUI::Window.new('detection')
+window.show(image)
+GUI::wait_key
