f5fc2b2802dbf658ad29b2b136cac2d0b17f7fe5
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-23 22:47:38 -0400

    opencv

--- opencv_module.rb
+++ opencv_module.rb
@@ -2,15 +2,15 @@ require 'opencv'
 include OpenCV
 system("python GrabImage.py") # This will return true
 
-if ARGV.length < 2
-  puts "Usage: ruby #{__FILE__} source dest"
-  exit
-end
+#if ARGV.length < 2
+#  puts "Usage: ruby #{__FILE__} source dest"
+#  exit
+#end
 
-data = './opencv_data/haarcascades/haarcascade_frontalface_alt.xml'
+data = '/home/pi/rc9k/opencv_data/haarcascades/haarcascade_frontalface_alt.xml'
 detector = CvHaarClassifierCascade::load(data)
 #image = CvMat.load(ARGV[0])
-image = CvMat.load("./image.png")
+image = CvMat.load("image.png")
 detector.detect_objects(image).each do |region|
   color = CvColor::Blue
   image.rectangle! region.top_left, region.bottom_right, :color => color
