438aed3246e9fe9ef96968f2165dcd23d29950bd
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-24 19:28:55 -0400

    ruby get python stuff

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,22 +1,6 @@
 
-require 'opencv'
+output = `ball_tracking.py`
 
-include OpenCV
-system("python GrabImage.py") # This will return true
 
-original_window = GUI::Window.new "original"
-hough_window = GUI::Window.new "hough circles"
 
-image = IplImage::load "image.png"
-gray = image.BGR2GRAY
-
-result = image.clone
-original_window.show image
-detect = gray.hough_circles(CV_HOUGH_GRADIENT, 2.0, 10, 200, 50)
-puts detect.size
-detect.each{|circle|
-  puts "#{circle.center.x},#{circle.center.y} - #{circle.radius}"
-  result.circle! circle.center, circle.radius, :color => CvColor::Red, :thickness => 3
-}
-hough_window.show result
-GUI::wait_key
+puts "#{{output}}"
