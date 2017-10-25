f7be79b64abccf64822c2e12a8f8faacf3c26f8e
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-24 21:23:44 -0400

    file.open

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,10 +1,9 @@
-#!/usr/bin/env ruby
-
-require 'file/tail'
-
-filename = ARGV.pop or fail "Usage: #$0 number filename"
-number = (ARGV.pop || 0).to_i.abs
-
-File::Tail::Logfile.open(filename) do |log|
-  log.backward(number).tail { |line| puts line }
+require 'thread'
+process_this = thread.new {
+while true
+  File.open("./tmp/tempcamera.txt").each do |line|
+    puts line
+  end
+  sleep 0.25
 end
+}
