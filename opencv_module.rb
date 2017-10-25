f697e776d40e76b1b1036a982f6c182180383e39
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-24 21:08:20 -0400

    manual tail

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,9 +1,1 @@
-require "rubygems"
-require "filewatch/tail"
-
-t = FileWatch::Tail.new
-t.tail("/home/pi/rc9k/tmp/tempcamera.txt")
-puts "#{t}"
-t.subscribe do |path, line|
-  puts "#{path}: #{line}"
-end
+results = `tail -f ./tmp/tempcamera.txt`
