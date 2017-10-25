daae6fad397d0dd059c341cace3bce94ccf2ac33
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-24 20:41:21 -0400

    retreive data from tempcamera.txt

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,5 +1,9 @@
 
-output = `python ball_tracking.py`
-#while !output.blank?
-  puts "#{output}"
-#end
+require 'file/tail'
+
+filename = ARGV.pop or fail "Usage: #$0 number filename"
+number = (ARGV.pop || 0).to_i.abs
+
+File::Tail::Logfile.open(filename) do |log|
+  log.backward(number).tail { |line| puts line }
+end
