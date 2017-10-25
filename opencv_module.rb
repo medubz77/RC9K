ad78a78a6f40096af474c7593cff7acd035f2b05
Rob Mink <rob.mink@gmail.com>
2017-10-24 21:57:11 -0400

    poulling again

--- opencv_module.rb
+++ opencv_module.rb
@@ -1,13 +1,13 @@
 require 'thread'
 class ReadData
-def startpython
+self.def startpython
   runthis= Thread.new{
 something=exec("python ball_tracking.py")
   }
 end
 
- def get_tempcamera_data
-runme=Thread.new{
+ self.def get_tempcamera_data
+runme = Thread.new{
   while (true)
     File.open("./tmp/tempcamera.txt").each do |line|
       $rawcamera=line
