9aafa031db99b6400b29c8fdb5afdced5a72cd0c
Waldo Ramirez <waldoxwho@yahoo.com>
2017-10-24 21:03:16 -0400

    chase tail

--- opencv_module.rb
+++ opencv_module.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "filewatch/tail"
 
 t = FileWatch::Tail.new
-t.tail("./tmp/tempcamera.txt")
+t.tail("/home/pi/rc9k/tmp/tempcamera.txt")
 t.subscribe do |path, line|
   puts "#{path}: #{line}"
 end
