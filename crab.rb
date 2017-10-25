4fc5a072a8ec8efe9fe1bccb0d3d2dd90bbac5b8
Rob Mink <rob.mink@gmail.com>
2017-10-24 22:23:27 -0400

    end all be all

--- crab.rb
+++ crab.rb
@@ -69,8 +69,7 @@ elsif x<=100    			#sharp veer left
 		x=1-x
 		x=x*-1
 		complex_walk(1,0.1,15,15,x,1)
-elsif (x>280)&&(x<380)   #veer right
-	if (x<200)&&(x>100)
+elsif (x>280)&&(x<380)   #veer right)
 		x=x-100
 		x=x/100.0
 		complex_walk(1,0.1,15,15,1,x)
@@ -85,7 +84,7 @@ elsif (x>280)&&(x<380)   #veer right
 end
 
 end
-
+end
 
 
 
