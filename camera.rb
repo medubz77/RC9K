a9473d678c12316e60736b4584df261f2ebe8f19
Waldo Ramirez via rc9k <waldoxwho@yahoo.com>
2018-02-10 20:03:31 -0500

    stuff opencv

--- camera.rb
+++ camera.rb
@@ -7,13 +7,15 @@ include OpenCV
 
 window = GUI::Window.new("face detect")
 capture = CvCapture.open
-detector = CvHaarClassifierCascade::load("./opencv_data/haarcascades/haarcascade_frontalface_alt.xml")
-
+facerec = File.expand_path("./opencv_data/haarcascades/haarcascade_frontalface_alt.xml")
+detector = CvHaarClassifierCascade::load("#{facerec}")
+#detector = CvHaarClassifierCascade::load("./opencv_data/haarcascades/haarcascade_frontalface_alt.xml")
+puts facerec
 loop {
   image = capture.query
-  detector.detect_objects(image).each { |rect|
-    image.rectangle! rect.top_left, rect.bottom_right, :color => CvColor::Red
-  }
+#  detector.detect_objects(image).each { |rect|
+#    image.rectangle! rect.top_left, rect.bottom_right, :color => CvColor::Red
+#  }
   window.show image
   break if GUI::wait_key(100)
 }
